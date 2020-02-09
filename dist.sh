a0674ab6cdc6b51609f957ed004f327fef87f1af
Minsu Kim <k09089@naver.com>
2020-02-09 18:29:47 +0900

    release 1.0.2

---
+++ dist.sh
@@ -0,0 +1,5 @@
+#!env/bin/activate
+
+rm dist/*
+python3 setup.py sdist bdist_wheel
+python3 -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
--- setup.py
+++ setup.py
@@ -2,11 +2,16 @@ from setuptools import setup, find_packages
 
 
 setup(name="datetime-quarter",
-      version="1.0.0",
+      version="1.0.3",
       url="https://github.com/BetaS/datetime-quarter",
       author="BetaS",
       author_email="thou1999@gmail.com",
       description="Simple and lightweight quarter support for python datetime",
       packages=find_packages(),
       long_description=open('README.md').read(),
+      long_description_content_type="text/markdown",
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License"
+      ]
 )
